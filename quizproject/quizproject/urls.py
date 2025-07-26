fbd0ec2952f90036277093478961868b1147a767
Vishwa Lingam <64265390+vishwa24816@users.noreply.github.com>
2025-07-26 05:08:07 +0000

    Add root URL redirect to /quiz/ and fix server accessibility issues

--- quizproject/quizproject/urls.py
+++ quizproject/quizproject/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('quiz/', include('quiz.urls')),
+    path('', RedirectView.as_view(url='/quiz/', permanent=False)),
 ]
